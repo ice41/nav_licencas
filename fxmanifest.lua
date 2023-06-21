86add4472f0815c584f9bbb6ec3b8b79bb10f329
ice41 <edu.ice7@gmail.com>
2023-06-21 19:56:23 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'ice41'
+contact 'E-mail: geral@ice41.pt - Discord: discord.gg/eQgRv2BfxT'
+version '1.0.4'
+
+ui_page 'nui/index.html'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'server.lua'
+}
+
+files {
+	'nui/index.html',
+	'nui/js.js',
+	'nui/css.css'
+}
